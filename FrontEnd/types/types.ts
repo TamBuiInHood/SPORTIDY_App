097e9564b26c6b36c693a610f04a45e13e1e4b37
inhoang2523 <yle405960@gmail.com>
2024-09-28 07:34:01 +0700

    add login flow

---
+++ FrontEnd/types/types.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+    Splash: any;
+    SignUp: any;
+    Login: any;
+    ForgotPassword: any;
+    Verification: any;
+    NewPassword: any;
+    UserProfile: any;
+    // Add other screen names and their params here
+  };
